b1a01f53a9c71e543e12cf78be3c4cba69104481
Noah Birnel <nbirnel@gmail.com>
2014-03-28 12:52:24 -0700

    passing initial tests

---
+++ lib/opticon.rb
@@ -0,0 +1,60 @@
+class Opticon
+  attr_reader :docs, :records
+
+  def initialize input
+    docs = []
+    doc = []
+    records = []
+    record = {}
+    while line = input.gets
+      record = Hash.new nil
+      l = line.chomp.split(',')
+      record = {
+        :key    => l[0],
+        :volume => l[1],
+        :path   => l[2],
+        :break  => l[3],
+        :empty1 => l[4],
+        :empty2 => l[5],
+        :pages  => l[6],
+      }
+      if record[:break]
+        docs.push doc if doc != []
+        doc = []
+      end
+      doc.push record
+      records.push record
+    end
+    doc.push record
+    docs.push doc if doc != []
+    @records = records
+    @docs = docs
+  end
+
+  def beg_end
+    b = @docs[0][0][:key]
+    e = @docs[-1][-1][:key]
+    [b, e]
+  end
+
+  def doc_count
+    @docs.length
+  end
+
+  def image_count
+    @records.length
+  end
+
+  def tiff_count
+    @records.select{|r| r[:path] =~ /\.tif$/}.length
+  end
+
+  def jpeg_count
+    @records.select{|r| r[:path] =~ /\.jpg$/}.length
+  end
+
+  def volumes
+    @records.map{|r| r[:volume]}.uniq
+  end
+
+end
--- spec/opicon_spec.rb
+++ spec/opicon_spec.rb
@@ -3,14 +3,31 @@ require  "#{File.dirname(__FILE__)}/../lib/opticon"
 describe Opticon do
 
   before do
+    @opt = Opticon.new(File.open("#{File.dirname(__FILE__)}/../sample/sample.opt"))
   end
 
-  describe Opticon.new do
+  it 'returns a beg - end' do
+    @opt.beg_end.should eq ["CTRL-000001", "CTRL-000018"]
+  end
 
-    #it 'dereferences windows network mounts' do
-    #  @dpaths[:windows].win_deref.windows.should eq @drives[:windows]
-    #end
+  it 'returns a document count' do
+    @opt.doc_count.should eq 3
+  end
 
+  it 'returns an image count' do
+    @opt.image_count.should eq 18
+  end
+
+  it 'returns a tiff count' do
+    @opt.tiff_count.should eq 17
+  end
+
+  it 'returns a jpeg count' do
+    @opt.jpeg_count.should eq 1
+  end
+
+  it 'returns an list of volumes' do
+    @opt.volumes.should eq ["SAMPLE_VOLUME_001", "SAMPLE_VOLUME_002"]
   end
 
 end
