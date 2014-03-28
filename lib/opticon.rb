3feca6ecd044ac48fd58e8d8b1f3e9f0eaeeec2d
Noah Birnel <nbirnel@gmail.com>
2014-03-28 14:52:27 -0700

    factor out parsing

--- lib/opticon.rb
+++ lib/opticon.rb
@@ -1,56 +1,65 @@
-class Opticon
-  attr_reader :docs, :records
+module Opticon
 
-  def initialize input
-    @docs      = []
-    c_doc     = []
-    @records   = []
-    while line = input.gets
-      l = line.chomp.split(',')
-      c_record = {
-        :key    => l[0],
-        :volume => l[1],
-        :path   => l[2],
-        :break  => l[3],
-        :empty1 => l[4],
-        :empty2 => l[5],
-        :pages  => l[6],
-      }
-      if c_record[:break]
-        @docs.push c_doc if c_doc != []
-        c_doc = []
-      end
-      c_doc.push   c_record[:key]
-      @records.push c_record
+  class Opticon
+    attr_reader :records
+
+    def initialize  records
+      @records = records
     end
-    c_doc.push c_record[:key]
-    @docs.push c_doc if c_doc != []
+
+    def beg_end
+      b = @records[0][:key]
+      e = @records[-1][:key]
+      [b, e]
+    end
+
+    def doc_count
+      @records.select{|r| r[:break]}.length
+    end
+
+    def image_count
+      @records.length
+    end
+
+    def tiff_count
+      @records.select{|r| r[:path] =~ /\.tif$/}.length
+    end
+
+    def jpeg_count
+      @records.select{|r| r[:path] =~ /\.jpg$/}.length
+    end
+
+    def volumes
+      @records.map{|r| r[:volume]}.uniq
+    end
+
   end
 
-  def beg_end
-    b = @records[0][:key]
-    e = @records[-1][:key]
-    [b, e]
-  end
+  class Parser
 
-  def doc_count
-    @docs.length
-  end
+    def initialize
+      @docs      = []
+      @records   = []
+    end
 
-  def image_count
-    @records.length
-  end
+    def parse input
+      c_doc     = []
+      while line = input.gets
+        l = line.chomp.split(',')
+        c_record = {
+          :key    => l[0],
+          :volume => l[1],
+          :path   => l[2],
+          :break  => l[3],
+          :empty1 => l[4],
+          :empty2 => l[5],
+          :pages  => l[6],
+        }
+        @records.push c_record
+      end
+      Opticon.new  @records
+    end
 
-  def tiff_count
-    @records.select{|r| r[:path] =~ /\.tif$/}.length
-  end
-
-  def jpeg_count
-    @records.select{|r| r[:path] =~ /\.jpg$/}.length
-  end
-
-  def volumes
-    @records.map{|r| r[:volume]}.uniq
   end
 
 end
---
+++ spec/opticon_spec.rb
@@ -0,0 +1,40 @@
+require  "#{File.dirname(__FILE__)}/../lib/opticon"
+
+describe Opticon do
+
+  describe Opticon::Opticon do
+
+    before do
+      @parser = Opticon::Parser.new
+      @sample = "#{File.dirname(__FILE__)}/../sample/sample.opt"
+      @io = File.new(@sample,'r')
+      @opt = @parser.parse @io
+    end
+
+    it 'returns a beg - end' do
+      @opt.beg_end.should eq ["CTRL-000001", "CTRL-000018"]
+    end
+
+    it 'returns a document count' do
+      @opt.doc_count.should eq 3
+    end
+
+    it 'returns an image count' do
+      @opt.image_count.should eq 18
+    end
+
+    it 'returns a tiff count' do
+      @opt.tiff_count.should eq 17
+    end
+
+    it 'returns a jpeg count' do
+      @opt.jpeg_count.should eq 1
+    end
+
+    it 'returns an list of volumes' do
+      @opt.volumes.should eq ["SAMPLE_VOLUME_001", "SAMPLE_VOLUME_002"]
+    end
+
+  end
+
+end
