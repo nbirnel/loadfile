89f7c1638913fac0ec09d0219621f3708cb45dec
Noah Birnel <nbirnel@gmail.com>
2014-03-28 16:23:54 -0700

    add rdoc

--- lib/opticon.rb
+++ lib/opticon.rb
@@ -1,42 +1,75 @@
+##
+# classes for manipulating Concordance® Opticon® image loadfiles.
+
 module Opticon
+
+  ##
+  # Represent the data in a Concordance® Opticon® image loadfile.
 
   class Opticon
     attr_reader :records
 
+    ## 
+    # Create a new opticon composed of +records+.
+    
     def initialize records
       @records = records
     end
 
+    ## 
+    # Return the first and last image keys
+    
     def beg_end
       b = @records[0][:key]
       e = @records[-1][:key]
       [b, e]
     end
 
+    ##
+    # Return a list of begining and end image keys for each document. 
+    # FIXME not yet implemented.
+    
     def beg_end_list
-      
     end
 
+    ## 
+    # Return a count of documents.
+    
     def doc_count
       @records.select{|r| r[:break]}.length
     end
 
+    ## 
+    # Return a count of images
+    
     def image_count
       @records.length
     end
 
+    ## 
+    # Return a count of tiffs
+    
     def tiff_count
       @records.select{|r| r[:path] =~ /\.tif$/}.length
     end
 
+    ## 
+    # Return a count of jpegs
+    
     def jpeg_count
       @records.select{|r| r[:path] =~ /\.jpg$/}.length
     end
 
+    ## 
+    # Return a list of volumes
+    
     def volumes
       @records.map{|r| r[:volume]}.uniq
     end
 
+    ##
+    # Return a new opticon data stream, suitable for loading.
+    
     def to_s
       l = @records.map{|r| r.values.join ','}
       l.join("\r\n") << "\r\n"
@@ -44,11 +77,22 @@ module Opticon
 
   end
 
+  ##
+  # This class converts an opticon text file or data stream to an instance 
+  # of Opticon
+  
   class Parser
+
+    ##
+    # Create a new parser for Opticon
 
     def initialize
       @records   = []
     end
+
+    ##
+    # Parse text from IO object +input+. It is the caller's responsibility to
+    # open and close the +input+ correctly.
 
     def parse input
       c_doc     = []
